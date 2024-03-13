4a65b23940aa8f2a9211b8d5a82054744748b34f
Emilion <emiljadersten@gmail.com>
2024-03-13 15:59:04 +0100

    Try different axios version

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // https://emils-drf-api-8546c6776dcb.herokuapp.com
-axios.defaults.baseURL = "https://emils-drf-api-8546c6776dcb.herokuapp.com";
+axios.defaults.baseURL = "http://127.0.0.1:8000";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
