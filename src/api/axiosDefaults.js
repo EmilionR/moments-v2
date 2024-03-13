631e6215718ec12dae711113f3109d59a08d8538
Emilion <emiljadersten@gmail.com>
2024-03-13 09:37:37 +0100

    Add signin functionality

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+// https://emils-drf-api-8546c6776dcb.herokuapp.com
 axios.defaults.baseURL = "https://emils-drf-api-8546c6776dcb.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
