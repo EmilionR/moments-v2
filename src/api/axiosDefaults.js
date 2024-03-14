5897adb60b7bf7c6700e9e7ac60f43021da5967a
Emil Jädersten <emiljadersten@gmail.com>
2024-03-14 08:15:34 +0000

    Connect deployed API

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-// https://emils-drf-api-8546c6776dcb.herokuapp.com
-axios.defaults.baseURL = "https://8000-emilionr-emilsdrfapi-noo9k8scmiq.ws-eu110.gitpod.io";
+// https://emils-drf-api-8546c6776dcb.herokuapp.com/
+axios.defaults.baseURL = "https://emils-drf-api-8546c6776dcb.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
